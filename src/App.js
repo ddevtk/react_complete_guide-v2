974e6115b5df4e55d182b1a5ba2a9a04af2d1dc2
duongbui123 <buiducduong2342000@gmail.com>
2021-05-15 23:20:56 +0700

    fetching data from firebase, and fix some bug

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-import Cart from "./components/Cart/Cart";
-import Layout from "./components/Layout/Layout";
-import Products from "./components/Shop/Products";
-import { Fragment } from "react-is";
-import Notification from "./components/UI/Notification";
-import { sendCartData } from "./store/cart-slice";
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+import { Fragment } from 'react-is';
+import Notification from './components/UI/Notification';
+import { fetchCartData, sendCartData } from './store/cart-slice';
 
 let isShowNotificationFirstly = false;
 
 function App() {
-  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const showCart = useSelector(state => state.ui.cartIsVisible);
 
-  const notification = useSelector((state) => state.ui.notification);
+  const notification = useSelector(state => state.ui.notification);
+  const cart = useSelector(state => state.cart);
   const dispatch = useDispatch();
 
-  const cart = useSelector((state) => state.cart);
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, []);
 
   useEffect(() => {
     if (!isShowNotificationFirstly) {
       isShowNotificationFirstly = true;
       return;
     }
-    dispatch(sendCartData(cart));
+    if (cart.isShow) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
 
   return (
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,16 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { uiActions } from "./ui-slice";
+import { createSlice } from '@reduxjs/toolkit';
+import { uiActions } from './ui-slice';
 
 const cartSlice = createSlice({
-  name: "cart",
+  name: 'cart',
   initialState: {
     items: [],
     totalQuantity: 0,
+    isShow: false,
   },
   reducers: {
+    replaceData(state, action) {
+      state.items = action.payload.items;
+      state.totalQuantity = action.payload.totalQuantity;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
-      const existingItem = state.items.find((item) => item.id === newItem.id);
+      const existingItem = state.items.find(item => item.id === newItem.id);
       state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
@@ -24,59 +29,104 @@ const cartSlice = createSlice({
         existingItem.quantity++;
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
-      console.log(existingItem);
+      state.isShow = true;
     },
     removeItemFromCart(state, action) {
       const id = action.payload;
-      const existingItem = state.items.find((item) => item.id === id);
+      const existingItem = state.items.find(item => item.id === id);
       state.totalQuantity--;
       if (existingItem.quantity === 1) {
-        state.items = state.items.filter((item) => item.id !== id);
+        state.items = state.items.filter(item => item.id !== id);
       } else {
         existingItem.quantity--;
+        existingItem.totalPrice -= action.payload.price;
       }
+      state.isShow = true;
     },
   },
 });
 
-export const sendCartData = (cart) => {
-  return async (dispatch) => {
-    dispatch(
-      uiActions.showNotification({
-        status: "Pending...",
-        title: "Sending...",
-        message: "Sending cart data !",
-      })
-    );
-
-    const sendRequest = async () => {
-      const res = await fetch(
-        "https://send-http-request-guide-default-rtdb.firebaseio.com/carts.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!res.ok) {
-        throw new Error("Sending data failed!");
-      }
-    };
-
+export const fetchCartData = () => {
+  return async dispatch => {
     try {
-      await sendRequest();
       dispatch(
         uiActions.showNotification({
-          status: "success 🚀",
-          title: "Success 🚀",
-          message: "Sending cart data successfully 🚀",
+          status: 'Fetching...',
+          title: 'Fetching...',
+          message: 'Fetching cart data ... !',
+        })
+      );
+      const res = await fetch(
+        'https://send-http-request-guide-default-rtdb.firebaseio.com/carts.json'
+      );
+      if (!res.ok) {
+        throw new Error('Fetching data failed');
+      }
+      const data = await res.json();
+      console.log(data);
+      dispatch(
+        cartActions.replaceData({
+          items: data.items || [],
+          totalQuantity: data.totalQuantity,
+        })
+      );
+      dispatch(
+        uiActions.showNotification({
+          status: 'success 🚀',
+          title: 'Success 🚀',
+          message: 'Fetching cart data successfully 🚀',
         })
       );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
-          status: "error",
-          title: "Error",
-          message: "Sending data failed !",
+          status: 'error',
+          title: 'Error',
+          message: 'Fetching data failed !',
+        })
+      );
+    }
+  };
+};
+
+export const sendCartData = cart => {
+  return async dispatch => {
+    try {
+      dispatch(
+        uiActions.showNotification({
+          status: 'Pending...',
+          title: 'Sending...',
+          message: 'Sending cart data !',
+        })
+      );
+
+      const res = await fetch(
+        'https://send-http-request-guide-default-rtdb.firebaseio.com/carts.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
+      );
+      if (!res.ok) {
+        throw new Error('Sending data failed!');
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success 🚀',
+          title: 'Success 🚀',
+          message: 'Sending cart data successfully 🚀',
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error',
+          message: 'Sending data failed !',
         })
       );
     }
