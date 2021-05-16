63374aec1c788b8726c8d435e8c4d040208ec58b
ddevtk <buiducduong2342000@gmail.com>
2021-05-16 21:44:22 +0700

    initial app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-import './App.css';
-import MainHeader from './components/main-header/main-header.cpn';
-import { Route, Switch } from 'react-router';
-import Welcome from './pages/welcome/welcome.cpn';
-import Product from './pages/product/product.cpn';
-import ProductDetail from './pages/product-detail/product-detail.cpn';
-
 function App() {
   return (
-    <div className='App'>
-      <MainHeader />
-      <Switch>
-        <Route path='/welcome' component={Welcome} />
-        <Route exact path='/products' component={Product} />
-        <Route path='/products/:ProductId' component={ProductDetail} />
-      </Switch>
+    <div>
+
     </div>
   );
 }
--- src/pages/welcome/welcome.cpn.jsx
+++
@@ -1,7 +0,0 @@
-const Welcome = () => (
-  <div>
-    <h1>Welcome to page</h1>
-  </div>
-);
-
-export default Welcome;
